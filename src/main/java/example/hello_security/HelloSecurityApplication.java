8ff185bec95d613c07a44227db1d1ebf7363be0d
J-Lucien <jtolojanahary5@gmail.com>
2024-12-28 19:39:30 +0300

    Remove unused code in main

--- src/main/java/example/hello_security/HelloSecurityApplication.java
+++ src/main/java/example/hello_security/HelloSecurityApplication.java
@@ -1,13 +1,7 @@
 package example.hello_security;
 
-import example.hello_security.ip.FailedAttempt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.concurrent.ConcurrentHashMap;
 
 @SpringBootApplication
 public class HelloSecurityApplication {
@@ -16,13 +10,4 @@ public class HelloSecurityApplication {
 		SpringApplication.run(HelloSecurityApplication.class, args);
 	}
 
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//	}
-
-	@Bean(name = "attemptConcurrentHashMap")
-	public ConcurrentHashMap<String, FailedAttempt> attemptConcurrentHashMap() {
-		return new ConcurrentHashMap<>();
-	}
 }
