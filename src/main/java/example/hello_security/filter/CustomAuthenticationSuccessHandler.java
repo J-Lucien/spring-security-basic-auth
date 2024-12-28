d43943ec8eb54c9f8ac24fe44074b5b80a1b6513
J-Lucien <jtolojanahary5@gmail.com>
2024-12-28 11:04:02 +0300

    Implement custom filters

---
+++ src/main/java/example/hello_security/filter/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,22 @@
+package example.hello_security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.context.SecurityContextRepository;
+
+import java.io.IOException;
+
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        response.sendRedirect("/home");
+    }
+}
---
+++ src/main/java/example/hello_security/filter/exception/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,20 @@
+package example.hello_security.filter.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+        String ipAddress = request.getRemoteAddr();
+        System.out.println("Login failed from IP: {}"+ ipAddress);
+        response.sendRedirect("/login?error=true");
+    }
+}
